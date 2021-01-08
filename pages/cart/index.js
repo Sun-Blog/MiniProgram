18aa86615b6b4cc748849945bfe7203deb68ed1a
w_wangtiexin <w_wangtiexin@youzan.com>
2021-01-08 17:53:36 +0800

    新增购物车收货地址按钮

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -6,7 +6,7 @@ Page({
    */
   data: {
 
-  },
+  }, 
 
   /**
    * 生命周期函数--监听页面加载
@@ -15,52 +15,29 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 点开收货地址按钮
+  handleChooseAddress(){
+    wx.getSetting({
+      success: (result)=>{
+        const scopeAddress = result.authSetting["scope.address"];
+        if (scopeAddress===true || scopeAddress===undefined) {
+          wx.chooseAddress({
+            success: (result1)=>{
+              console.log(result1);
+            }
+          })
+        }else{
+          wx.openSetting({
+            success: (result2)=>{
+              wx.chooseAddress({
+                success: (result3)=>{
+                  console.log(result3);
+                }
+              })
+            }
+          });
+        }
+      }
+    });
   }
 })
